d22c5bba38cda8562727c662e826f00e2605ee50
RomanAgne <53412239+RomanAgne@users.noreply.github.com>
2020-10-24 11:14:33 -0700

    Update Duck (First program).py
    Cleaned up the code and got rid of uneccesary lines.

--- Duck (First program).py
+++ Duck (First program).py
@@ -6,10 +6,8 @@ No = 'No'
 
 q1 = input('Do you have feathers?(Yes or No): ')
 if q1 == 'Yes':
-    Yes == True
     duckPerc+= 20
 elif q1 == 'No':
-    No == False
     duckPerc += 0
 else:
     print(input('Please say Yes or No: '))
@@ -18,10 +16,8 @@ else:
 
 q2 = input('Do you have a corkscrew penis?(Yes or No): ')
 if q2 == 'Yes':
-    Yes == True
     duckPerc += 20
 elif q2 == 'No':
-    No == False
     duckPerc += 0
 else:
     print(input('Please say Yes or No: '))
@@ -30,10 +26,8 @@ else:
 
 q3 = input('Do you caw?(Yes or No): ')
 if q3 == 'Yes':
-    Yes == False
     duckPerc += 0
 elif q3 == 'No':
-    No == True
     duckPerc +=20
 else:
     print(input('Please say Yes or No: '))
@@ -42,10 +36,8 @@ else:
 
 q4 = input('Have you gone quackers?(Yes or No): ')
 if q4 == 'Yes':
-    Yes == False
     duckPerc += 0
 elif q4 == 'No':
-    No == True
     duckPerc += 20
 else:
     print(input('Please say Yes or No: '))
@@ -53,24 +45,13 @@ else:
 #Question 5
 q5 = input('Are you being watched?(Yes or No): ')
 if q5 == 'Yes':
-    Yes == True
     duckPerc += 20
 elif q5 == 'No':
-    No == False
     duckPerc += 0
 else:
     print(input('Please say Yes or No: '))
 
-class Duck:
-    if duckPerc == 100:
-        print('You are 100% Duck.')
-    elif duckPerc == 80:
-        print('You are 80% Duck.')
-    elif duckPerc == 60:
-        print('You are 60% Duck.')
-    elif duckPerc == 40:
-        print('You are 40% Duck.')
-    elif duckPerc == 20:
-        print('You are 20% Duck.')
-    else:
-        print('You are not a Duck.')
+if duckPerc:
+    print(f"You are {duckPerc}% Duck.")
+else:
+    print("You are not a duck")
